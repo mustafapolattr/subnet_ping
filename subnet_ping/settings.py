f8f33f63363c4c23a79511fdc3d3a066e82c92cf
MPolat <mustfplt4@gmail.com>
2024-09-12 15:00:53 +0300

    allowed hosts update in settings

--- subnet_ping/settings.py
+++ subnet_ping/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(=16on$v@aj@0sy%i(5$6^ph@5j5o$d0qg55#-uc#y8o6oga+o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
